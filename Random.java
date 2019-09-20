e4b6542ce02dfaa020861f1256ac7a25c56ecc70
Mila Arkhiptsova <40221785+arkhiptsova@users.noreply.github.com>
2019-09-19 22:39:49 -0500

    Update Random.java

--- Random.java
+++ Random.java
@@ -2,12 +2,11 @@ package FeetNumber;
 
 
 /*
-Mila Arkhiptcova
+Mila 
 This program randomly generates an integer between 1 and 12 and 
 displays the English month names January, February, ... December for the numbers 
 1,2, ... 12, accordingly.
-09/2/2019
-Bellevue University
+
 Random.java
 */
 
